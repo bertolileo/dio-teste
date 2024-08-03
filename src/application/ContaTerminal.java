0116490a966514bcfb64f043c4a9da86d5c976e0
Leonardo Bertoli <leobertoli2091@gmail.com>
2024-08-03 05:10:21 -0300

    Finalizando

--- src/application/ContaTerminal.java
+++ src/application/ContaTerminal.java
@@ -1,5 +1,51 @@
 package application;
 
 public class ContaTerminal {
+	private int numero;
+	private String agencia;
+	private String nome;
+	private Double saldo;
+	
+	public ContaTerminal() {
+	}
 
+	public ContaTerminal(int numero, String agencia, String nome, Double saldo) {
+		this.numero = numero;
+		this.agencia = agencia;
+		this.nome = nome;
+		this.saldo = saldo;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(String agencia) {
+		this.agencia = agencia;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(Double saldo) {
+		this.saldo = saldo;
+	}
+	
 }
